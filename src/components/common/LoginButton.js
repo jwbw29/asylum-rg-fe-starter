c9c19b54f6f1cb8582999b33c8d1b562f9cf4734
Justin Byrd <justinbyrd7@gmail.com>
2024-01-16 13:40:41 -0600

    Refactor login button styling

--- src/components/common/LoginButton.js
+++ src/components/common/LoginButton.js
@@ -5,20 +5,7 @@ import '../../styles/LoginPage.less';
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <button
-      // style={{
-      //   backgroundColor: '#f5f5f5',
-      //   color: '#000000',
-      //   border: 'none',
-      //   borderRadius: '5px',
-      //   padding: '10px',
-      //   fontSize: '20px',
-      //   fontWeight: 'bold',
-      //   cursor: 'pointer',
-      // }}
-      onClick={() => loginWithRedirect()}
-      id="LoginButton"
-    >
+    <button onClick={() => loginWithRedirect()} id="login-button">
       Log In
     </button>
   );
