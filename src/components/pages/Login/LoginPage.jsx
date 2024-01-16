3e05da069d22b592ddbe5af4960e6cea65a82fd8
Justin Byrd <justinbyrd7@gmail.com>
2024-01-15 20:05:35 -0600

    Add login form and styling to LoginPage.jsx

--- src/components/pages/Login/LoginPage.jsx
+++ src/components/pages/Login/LoginPage.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import LoginButton from '../../common/login-button';
+import '../../../styles/LoginPage.less';
 
 const LoginPage = () => {
   return (
     <div>
-      <h1>Login Page</h1>
+      <div className="header">
+        <div className="header-text-container">
+          <h1>Asylum Office Grant Rate Tracker</h1>
+          <h3>
+            The Asylum Office Grant Rate Tracker provides asylum seekers,
+            researchers, policymakers, and the public an interactive tool to
+            explore USCIS data on Asylum Office decisions
+          </h3>
+        </div>
+      </div>
+      <div className="middle-section">
+        <div className="login-form">
+          <h1>Login</h1>
+          <form>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+            <LoginButton />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
