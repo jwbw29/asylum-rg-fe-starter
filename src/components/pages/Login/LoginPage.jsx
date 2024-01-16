4f8c9172de72dd74a00dbefa5315c83dccc42c66
Justin Byrd <justinbyrd7@gmail.com>
2024-01-15 20:29:37 -0600

    Refactor login form layout and update button component

--- src/components/pages/Login/LoginPage.jsx
+++ src/components/pages/Login/LoginPage.jsx
@@ -4,28 +4,22 @@ import '../../../styles/LoginPage.less';
 
 const LoginPage = () => {
   return (
-    <div>
-      <div className="header">
-        <div className="header-text-container">
-          <h1>Asylum Office Grant Rate Tracker</h1>
-          <h3>
-            The Asylum Office Grant Rate Tracker provides asylum seekers,
-            researchers, policymakers, and the public an interactive tool to
-            explore USCIS data on Asylum Office decisions
-          </h3>
-        </div>
-      </div>
-      <div className="middle-section">
-        <div className="login-form">
-          <h1>Login</h1>
-          <form>
+    <div className="form-container">
+      <div className="login-form">
+        <h1>Login</h1>
+        <form>
+          <div className="email">
+            {' '}
             <label htmlFor="email">Email</label>
             <input type="email" id="email" />
+          </div>
+          <div className="password">
+            {' '}
             <label htmlFor="password">Password</label>
             <input type="password" id="password" />
-            <LoginButton />
-          </form>
-        </div>
+          </div>
+          <LoginButton className="login-button" />
+        </form>
       </div>
     </div>
   );
