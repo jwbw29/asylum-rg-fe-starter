9e1dbebe3e0ce8c2f8c43c182689795abf83d12d
Justin Byrd <justinbyrd7@gmail.com>
2024-01-17 11:48:13 -0600

    Refactor LoadingPage and ProfilePage components

--- src/components/common/LoadingPage.jsx
+++ src/components/common/LoadingPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../../styles/LoadingPage.less';
 
 export default function LoadingPage() {
   return (
-    <div>
-      <h1>Loading...</h1>
+    <div className="container">
+      <div className="loading-container">
+        <h1>Loading...</h1>
+        <div className="loader"></div>
+      </div>
     </div>
   );
 }
--- src/components/pages/Profile/ProfilePage.jsx
+++ src/components/pages/Profile/ProfilePage.jsx
@@ -1,47 +1,58 @@
-// import React from 'react';
-
-// const ProfilePage = () => {
-//   return (
-//     <div>
-//       {' '}
-//       {/*Container for Profile Page*/}
-//       <div>
-//         {/* Container for content */}
-//         <h1>Profile</h1>
-//         <div>
-//           {/*Container for details*/}
-//           <h2>Username</h2>
-//           <h2>Name</h2>
-//           <h2>Image</h2>
-//           {/* <img>User Photo</img> */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProfilePage;
-
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import '../../../styles/ProfilePage.less';
 
 const ProfilePage = () => {
   const { user, isAuthenticated } = useAuth0();
   return (
     isAuthenticated && (
-      <article>
-        {user?.picture && <img src={user.picture} alt={user?.name} />}
-        <h2>{user?.name}</h2>
-        <ul>
-          {Object.keys(user).map((objKey, i) => (
-            <li key={i}>
-              {objKey}: {user[objKey]}
-            </li>
-          ))}
-        </ul>
-      </article>
+      <div className="section-container">
+        <div className="content-container">
+          {/* <div className="profile-photo"> */}{' '}
+          {user?.picture && <img src={user.picture} alt={user?.name} />}
+          {/* </div> */}
+          <h2>{user?.name}</h2>
+          <div className="user-info">
+            <span className="obj-key">Username: </span>
+            <span className="user-value">{user.nickname}</span>
+          </div>
+          <div className="user-info">
+            <span className="obj-key">Sub: </span>
+            <span className="user-value">{user.sub}</span>
+          </div>
+          <div className="user-info">
+            <span className="obj-key">Last Updated: </span>
+            <span className="user-value">{user.updated_at}</span>
+          </div>
+        </div>
+      </div>
     )
   );
 };
 
 export default ProfilePage;
+
+// user object
+//  {
+//   "sub": "github|71021894",
+//   "nickname": "jwbw29",
+//   "name": "Justin Byrd",
+//   "picture": "https://avatars.githubusercontent.com/u/71021894?v=4",
+//   "updated_at": "2024-01-12T22:22:06.916Z"
+//   }
+// Would then apply this logic
+/* {Object.keys(user).map((objKey, i) => (
+    <div>
+      <span className="obj-key">Username: </span>
+      <span className="user-value">{user.nickname}</span>
+    </div>
+    <div>
+      <span className="obj-key">Sub: </span>
+      <span className="user-value">{user.sub}</span>
+    </div>
+    <div>
+      <span className="obj-key">Last Updated: </span>
+      <span className="user-value">{user.updated_at}</span>
+    </div>
+
+            ))} */
